40225ae63ede45ba930ae6426a71a5756dd80a25
Dr. Dos <doctordos@gmail.com>
2020-02-23 14:09:44 -0800

    Rename Middleware variable for modern Django

--- pmdu/settings.py
+++ pmdu/settings.py
@@ -85,7 +85,7 @@ TEMPLATES = [
 ]
 
 
-MIDDLEWARE_CLASSES = (
+MIDDLEWARE = (
     'django.middleware.common.CommonMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
